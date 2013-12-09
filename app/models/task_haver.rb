4b15bf31dfbe5ef6a02ccda98b4cbadb30f9377e
Matt Zafirovski <mattzaf@gmail.com>
2013-12-09 17:54:38 -0600

    Revert 388abc1..6179e80
    This rolls back to commit 388abc194dbc6246be99d98565b1c435ed98099b.

--- app/models/task_haver.rb
+++ app/models/task_haver.rb
@@ -1,4 +1,8 @@
 class TaskHaver < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 # belongs_to :director
 # has_many :roles
 # has_many :actors, :through => roles
