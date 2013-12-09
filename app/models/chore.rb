6747e3cafb636ea5d28ff5a6127902fffea0fe80
Matt Zafirovski <mattzaf@gmail.com>
2013-12-09 09:41:51 -0600

    Validation rule: New chore

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -7,8 +7,16 @@ class Chore < ActiveRecord::Base
  belongs_to :student
  belongs_to :type_of_chore
 
-  validates(:type_of_chore_id, :presence => true)
+  validates :task_haver_id, :presence =>true
+  validates :description_of_chore, :presence => true
+  validates :ask_price, :presence => true, :numericality => true
+  validates :complete_by, :presence => true
+
+
   # validates :director_id, :presence => true, :numbericality => { only_integer => true }
   # :numericality => {only integer: true} is another argument for this
   # validates :year, :presence => true, numbericality: { only_integer: tr} :uniqueness = > true
+
+
+
 end
