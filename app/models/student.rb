26ef645938fbea5637de2b8e0516aa4bb9f7839b
Matt Zafirovski <mattzaf@gmail.com>
2013-12-09 09:29:25 -0600

    Validation rule: New student

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,4 +10,11 @@ def full_name
     return first_name + " " + last_name
   end
 
+validates :first_name, :presence => true
+validates :last_name, :presence => true
+validates :address, :presence => true
+validates :cell_phone, :presence => true, :numericality => true, :uniqueness => true
+validates :nu_email, :uniqueness => true
+validates :student_id, :presence => true, :numericality => true, :uniqueness => true
+
 end
